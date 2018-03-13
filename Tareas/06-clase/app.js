5d0dbe2f8defb81b41b600d7b456b30cf94ac3cc
Christolml <christo_9722@hotmail.com>
2018-03-13 10:43:39 -0600

    Clase 6

---
+++ Tareas/06-clase/app.js
@@ -0,0 +1,54 @@
+
+// let person = {
+//     firstname: '',
+//     lastname: '',
+//     greet: function() {
+//         return this.firstname + ' ' + this.lastname;
+//     }
+// }
+
+// let Christo = Object.create(person);
+// Christo.firstname = 'Christopher';
+// Christo.lastname = 'Velazquez';
+
+// let ella = Object.create(person);
+// ella.firstname = 'Dana';
+// ella.lastname = 'Mora';
+
+
+// console.log(ella);
+// console.log(Christo);
+
+
+// se esta creando
+
+
+let EventEmitter = require('events');
+let util = require('util');
+
+function Greetr() {
+    this.greeting = 'Hello world!';
+}
+
+// con inherits puedo hacer que Greetr herede de EventEmitter
+util.inherits(Greetr, EventEmitter);
+
+// se esta metiendo con el prototipo y se le esta asignando una nuevaa funcionalidad
+Greetr.prototype.greet = function(data) {
+    console.log(`${this.greeting}: ${data}`);
+    // console.log(this.greeting + ':' + data);
+    this.emit('greet', data);
+}
+
+let greeter1 = new Greetr();
+
+greeter1.on('greet', function(data) {
+    console.log(`Someone greeted ${data}`);
+    // console.log('Somenone greeted: ' + data);
+});
+
+greeter1.greet('Christo');
+
+
+
+
